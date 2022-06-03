7a0db5273484c0d124d46eb060d22c6b9b8d7a14
Jacob Hearst <jacob.hearst@jamf.com>
2022-06-03 08:22:34 -0500

    Initial Commit

---
+++ MetroTransitBoard/MetroTransitBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroTransitBoardApp.swift
+//  MetroTransitBoard
+//
+//  Created by Jacob Hearst on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MetroTransitBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
